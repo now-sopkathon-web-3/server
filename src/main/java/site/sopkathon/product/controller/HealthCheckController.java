6d3aa7ded2dcfd9a72cedb8f5c03dd008b45ee81
unanchoi <unanc2@gmail.com>
2024-05-17 23:53:21 +0900

    setting: initial commit

--- src/main/java/site/sopkathon/product/controller/HealthCheckController.java
+++ src/main/java/site/sopkathon/product/controller/HealthCheckController.java
@@ -1,6 +1,9 @@
 package site.sopkathon.product.controller;
 
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,14 @@ import site.sopkathon.product.common.response.BaseResponse;
 
 @RestController
 @RequestMapping("/health")
+@Tag(name = "Health Check", description = "서버 상태 체크")
 public class HealthCheckController {
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "서버 상태 체크 성공")
+            }
+    )
     @GetMapping
     public ResponseEntity<BaseResponse<Void>> healthCheck() {
         return ResponseEntity.ok(BaseResponse.success(SuccessMessage.HEALTH_CHECK_SUCCESS));
