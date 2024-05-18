4f87c295933815cf4f911e37edd54f22fbfad1dd
unanchoi <unanc2@gmail.com>
2024-05-19 05:56:57 +0900

    fix: s3 path 변경

--- src/main/java/site/sopkathon/product/service/S3Service.java
+++ src/main/java/site/sopkathon/product/service/S3Service.java
@@ -37,7 +37,7 @@ public class S3Service {
 
         RequestBody requestBody = RequestBody.fromBytes(image.getBytes());
         s3Client.putObject(request, requestBody);
-        return "https://" + bucketName + ".ap-northeast-2.amazonaws.com/" + key;
+        return "https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/" + key;
     }
 
     public void deleteImage(String key) throws IOException {
