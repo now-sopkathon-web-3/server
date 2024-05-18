b40b3e02f19b5239cffd2b6ebb397c095e41254f
unanchoi <unanc2@gmail.com>
2024-05-18 23:15:32 +0900

    fix: @SpringBootTest 삭제

--- src/test/java/site/sopkathon/product/ProductApplicationTests.java
+++ src/test/java/site/sopkathon/product/ProductApplicationTests.java
@@ -3,11 +3,11 @@ package site.sopkathon.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProductApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class ProductApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
