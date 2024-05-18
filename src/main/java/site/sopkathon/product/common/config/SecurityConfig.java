70608d497b8c2f577dc8128c90f79aebb261cb68
unanchoi <unanc2@gmail.com>
2024-05-19 00:04:11 +0900

    feat: Cors 설정, JPA Auditing Config 구현

---
+++ src/main/java/site/sopkathon/product/common/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package site.sopkathon.product.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name())
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
