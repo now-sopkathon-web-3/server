1dce1cc3dc2d58190e63d36825bd3ecdcbd8fb82
unanchoi <unanc2@gmail.com>
2024-05-18 23:18:42 +0900

    feat: TimeZone 설정, BaseTimeEntity 구현

---
+++ src/main/java/site/sopkathon/product/common/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package site.sopkathon.product.common.entity;
+
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    public LocalDateTime createdAt;
+
+    @LastModifiedDate
+    public LocalDateTime updatedAt;
+}
+
