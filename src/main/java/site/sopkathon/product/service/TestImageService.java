0ed950b267befdac84a1da126e3c60934ec1f4d3
unanchoi <unanc2@gmail.com>
2024-05-18 22:26:59 +0900

    refactor: image 관련 업데이트 및 Swagger설정

--- src/main/java/site/sopkathon/product/service/TestImageService.java
+++ src/main/java/site/sopkathon/product/service/TestImageService.java
@@ -1,22 +1,31 @@
 package site.sopkathon.product.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import site.sopkathon.product.dto.request.TestImageRequest;
 
+import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class TestImageService {
+
+    private static final String S3_DIRECTORY = "testimages";
 
     private final S3Service service;
 
 
     @Transactional
-
     public void uploadTestImage(MultipartFile image, TestImageRequest request) {
-
+        try {
+            service.uploadImage(S3_DIRECTORY, image);
+        } catch (IOException e) {
+            log.error("Failed to upload image", e);
+        }
     }
 }
